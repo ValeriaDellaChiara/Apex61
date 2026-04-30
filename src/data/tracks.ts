04ce5cfc195956ac1cfd68c18b927f0683d257bb
ValeriaDellaChiara <73139003+ValeriaDellaChiara@users.noreply.github.com>
2026-04-30 16:37:03 +0200

    Update tracks.ts

--- src/data/tracks.ts
+++ src/data/tracks.ts
@@ -22,7 +22,8 @@ export const tracks: Track[] = [
     mapsUrl: 'https://maps.google.com/?q=Misano+World+Circuit',
     imageUrl: '/Misano.jpg',
     imageWidth: '410px', 
-    imageHeight: '250px'
+    imageHeight: '250px',
+    pricing: { individual: 850, group: 500 }
   },
   {
     id: 'magione',
@@ -32,7 +33,8 @@ export const tracks: Track[] = [
     mapsUrl: 'https://maps.google.com/?q=Autodromo+di+Magione',
     imageUrl: '/magione.png',
     imageWidth: '400px',
-    imageHeight: '250px'
+    imageHeight: '250px',
+    pricing: { individual: 700, group: 450 }
   },
   {
     id: 'pomposa',
@@ -42,7 +44,8 @@ export const tracks: Track[] = [
     mapsUrl: 'https://maps.google.com/?q=Autodromo+Pomposa',
     imageUrl: '/pomposa.png',
     imageWidth: '400px',
-    imageHeight: '250px'
+    imageHeight: '250px',
+    pricing: { individual: 850, group: 450 }
   },
   {
     id: 'mugello',
@@ -52,7 +55,8 @@ export const tracks: Track[] = [
     mapsUrl: 'https://maps.google.com/?q=Mugello+Circuit',
     imageUrl: '/mgello.jpeg',
     imageWidth: '400px',
-    imageHeight: '250px'
+    imageHeight: '250px',
+    pricing: { individual: 1000, group: 500 }
   },
   {
     id: 'cremona',
@@ -62,16 +66,18 @@ export const tracks: Track[] = [
     mapsUrl: 'https://maps.google.com/?q=Cremona+Circuit',
     imageUrl: '/cremona.png',
     imageWidth: '400px',
-    imageHeight: '250px'
+    imageHeight: '250px',
+    pricing: { individual: 450, group: 250 }
   },
   {
     id: 'vallelunga',
     name: 'Vallelunga',
     length: '4.085 km',
     location: 'Campagnano di Roma, Roma',
-    mapsUrl: 'https://maps.google.com/maps?vet=10CAAQoqAOahcKEwiY8PeamLSTAxUAAAAAHQAAAAAQCw..i&rlz=1C5AJCO_enIT1206IT1206&sca_esv=f339d775e9702b7a&pvq=CgovbS8wMjdoaDFjIhkKE2NpcmN1aXRvIHZhbGxlbHVuZ2EQAhgD&lqi=ChNjaXJjdWl0byB2YWxsZWx1bmdhSKv43iNaJRAAEAEYABgBIhNjaXJjdWl0byB2YWxsZWx1bmdhKgYIAhAAEAGSAQ5jYXJfcmFjZV90cmFjaw&fvr=1&cs=1&um=1&ie=UTF-8&fb=1&gl=it&sa=X&ftid=0x132f410e4080fdeb:0x12c20214365b5afa', 
+    mapsUrl: 'https://maps.google.com/maps?vet=10CAAQoqAOahcKEwiY8PeamLSTAxUAAAAAHQAAAAAQCw..i&rlz=1C5AJCO_enIT1206IT1206&sca_esv=f339d775e9702b7a&pvq=CgovbS8wMjdoaDFjIhkKE2NpcmN1aXRvIHZhbGxlbHVuZ2EQAhgD&lqi=ChNjaXJjdWl0byB2YWxsZWx1bmdhSKv43iNaJRAAEAEYABgBIhNjaXJjdWl0byB2YWxsZWx1bmdhKgYIAhAAEAGSAQ5jYXJfcmFjZV90cmFjaw&fvr=1&cs=1&um=1&ie=UTF-8&fb=1&gl=it&sa=X&ftid=0x132f410e4080fdeb:0x132f410e4080fdeb:0x12c20214365b5afa', 
     imageUrl: '/Vallelunga.png',
     imageWidth: '400px',
-    imageHeight: '250px'
+    imageHeight: '250px',
+    pricing: { individual: 800, group: 450 }
   },
 ];
